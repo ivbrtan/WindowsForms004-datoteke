673c0c2c066f1d1821b0b9cb4c8e2d55c5785d68
ivona.brtan <ivona.brtan@KABINF08>
2025-12-11 19:00:03 +0100

    ......

---
+++ WindowsForms005datoteke/Form3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms005datoteke
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
